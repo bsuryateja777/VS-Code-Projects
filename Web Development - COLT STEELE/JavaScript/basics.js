8985b074cfe59bb46f0b9a01e372b79a6daf8c43
suryateja <bsuryateja777@gmail.com>
2024-08-18 12:40:04 +0530

    completed

---
+++ Web Development - COLT STEELE/JavaScript/basics.js
@@ -0,0 +1,8 @@
+console.log("Hello from our first Js File")
+let age = prompt("please enter your age: ")
+if (age > 18) {
+    console.log("you are a major already")
+}
+else {
+    console.log("sorry, you are a minor")
+}
